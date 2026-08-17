7897cb246c596fe0bd59b5b4943eda94b26c5de0
VenusKing1026 <jinxing0033221@163.com>
2026-08-17 16:25:53 +0800

    build: add JSON-to-C++ PetriNet generator

---
+++ custom_modules/generated/xenophagy_model_generated.cpp
@@ -0,0 +1,232 @@
+// Generated file. Do not edit.
+// generator=1.0.0 model_sha256=beaafd39036a29aab6eb846082b6d4f2ceef830d7c2d83594d976bd681b5c305 integration_sha256=2d12690ac472de15b05f476dfa7e5e2b6603d042865fc20d6e768d506189c807
+#include "xenophagy_model_generated.h"
+#include <algorithm>
+#include <cmath>
+namespace xenophagy {
+const ModelParameters parameters{};
+const std::array<const char*, PLACE_COUNT> place_names = {{
+  "SalRuffle",
+  "SalVac",
+  "SalCyt",
+  "AdapSalCyt",
+  "CapCyt",
+  "AdapSalVac",
+  "CapVac",
+  "XenoSig",
+  "AA",
+  "AAstarvation",
+  "Ap_Gal8",
+  "Ap_Gal8_Ub",
+  "Ap_Gal8_Ub_N_S",
+  "Ap_Gal8_Ub_OPTNp",
+  "Ap_Ub",
+  "Ap_Ub_N_S",
+  "Ap_Ub_OPTNp",
+  "E3_ligase",
+  "Gal8",
+  "LC3_GABARAP",
+  "N_S",
+  "NDP52",
+  "OPTN",
+  "S_damagedSCV",
+  "S_Gal8",
+  "S_Gal8_NDP52",
+  "S_Gal8_Ub",
+  "S_Gal8_Ub_NDP52",
+  "S_Gal8_Ub_NDP52_N_S",
+  "S_Gal8_Ub_NDP52_OPTN_p62",
+  "S_Gal8_Ub_NDP52_OPTN_p62_N_S",
+  "S_Gal8_Ub_NDP52_OPTN_p62_N_S_diTBK1",
+  "S_Gal8_Ub_NDP52_OPTN_p62_N_S_diTBK1i",
+  "S_Gal8_Ub_NDP52_p62_OPTNp_N_S_OligoTBK1",
+  "S_Gal8_Ub_OPTN",
+  "S_Gal8_Ub_p62",
+  "S_Ub",
+  "S_Ub_NDP52",
+  "S_Ub_NDP52_N_S",
+  "S_Ub_NDP52_OPTN_p62",
+  "S_Ub_NDP52_OPTN_p62_N_S",
+  "S_Ub_NDP52_OPTN_p62_N_S_OligoTBK1",
+  "S_Ub_NDP52_OPTN_p62_N_S_diTBK1",
+  "S_Ub_NDP52_OPTN_p62_N_S_diTBK1i",
+  "S_Ub_OPTN",
+  "S_Ub_p62",
+  "SignalAutophagyInduction",
+  "SignalSCVdamage",
+  "TBK1",
+  "ULK1comp",
+  "mTORC1_ULK1comp",
+  "mTORC1_ULK1comp_SCV",
+  "mTORC1inactive",
+  "p62",
+  "LC3_signal",
+  "LC3",
+  "XenoStart",
+}};
+const std::array<Transition, 80> transitions = {{
+  {"StayingVac", 0.006, false, false, {{SalRuffle, 1}}, {{SalVac, 1}}},
+  {"EnteringCyt", 0.004, false, false, {{SalRuffle, 1}}, {{SalCyt, 1}}},
+  {"ProSalVac", 0.0001, true, true, {{AdapSalVac, 1}, {CapVac, 1}}, {{SalVac, 2}}},
+  {"ProSalCyt", 0.0001, true, true, {{AdapSalCyt, 1}, {CapCyt, 1}}, {{SalCyt, 2}}},
+  {"AdaptingCyt", 0.0167, false, true, {{SalCyt, 1}}, {{AdapSalCyt, 1}}},
+  {"AdaptingVac", 0.036, false, true, {{SalVac, 1}}, {{AdapSalVac, 1}}},
+  {"XenoDeg", 0.0001, true, true, {{SalCyt, 1}}, {{XenoSig, 1}, {SalCyt, 1}}},
+  {"VacDamage", 0.018, false, true, {{SalVac, 1}}, {{SalCyt, 1}}},
+  {"XenoDeg2", 0.0001, true, true, {{AdapSalCyt, 1}}, {{XenoSig, 1}, {AdapSalCyt, 1}}},
+  {"Deg1", 0.001, false, true, {{Ap_Gal8, 1}}, {}},
+  {"Deg2", 0.001, false, true, {{Ap_Gal8_Ub_OPTNp, 1}}, {}},
+  {"Deg2i", 0.001, false, true, {{Ap_Gal8_Ub_N_S, 1}}, {}},
+  {"Deg2ii", 0.001, false, true, {{Ap_Gal8_Ub, 1}}, {}},
+  {"Deg3", 0.001, false, true, {{Ap_Ub_OPTNp, 1}}, {}},
+  {"Deg3i", 0.001, false, true, {{Ap_Ub_N_S, 1}}, {}},
+  {"Deg3ii", 0.001, false, true, {{Ap_Ub, 1}}, {}},
+  {"Output", 0.001, false, true, {{mTORC1_ULK1comp_SCV, 1}}, {}},
+  {"Syn1", 0.0005, true, true, {}, {{Gal8, 1}}},
+  {"Syn2", 0.0005, true, true, {}, {{E3_ligase, 1}}},
+  {"Syn4", 0.0005, true, true, {}, {{p62, 1}}},
+  {"Syn5", 0.0005, true, true, {}, {{NDP52, 1}}},
+  {"Syn6", 0.0005, true, true, {}, {{OPTN, 1}}},
+  {"Syn7", 0.0005, true, true, {}, {{N_S, 1}}},
+  {"Syn8", 0.0005, true, true, {}, {{TBK1, 1}}},
+  {"Syn9", 0.0005, true, true, {}, {{mTORC1_ULK1comp, 1}}},
+  {"T1", 0.01, false, true, {{Gal8, 1}, {S_damagedSCV, 1}}, {{S_Gal8, 1}}},
+  {"T10", 0.01, false, true, {{N_S, 1}, {S_Gal8_Ub_NDP52, 1}}, {{S_Gal8_Ub_NDP52_N_S, 1}}},
+  {"T10i", 0.01, false, true, {{S_Gal8_Ub_NDP52_OPTN_p62, 1}, {N_S, 1}}, {{S_Gal8_Ub_NDP52_OPTN_p62_N_S, 1}}},
+  {"T11", 0.01, false, true, {{S_Gal8_Ub_NDP52_N_S, 1}}, {{N_S, 1}, {S_Gal8_Ub, 1}}},
+  {"T12", 0.01, false, true, {{N_S, 1}, {p62, 1}, {S_Gal8_Ub, 1}, {OPTN, 1}}, {{S_Gal8_Ub_NDP52_OPTN_p62_N_S, 1}}},
+  {"T13", 0.01, false, true, {{S_Gal8_Ub_NDP52_OPTN_p62_N_S, 1}, {TBK1, 2}}, {{S_Gal8_Ub_NDP52_OPTN_p62_N_S_diTBK1, 1}}},
+  {"T13i", 0.01, false, true, {{S_Gal8_Ub_NDP52_OPTN_p62_N_S, 1}, {TBK1, 2}}, {{S_Gal8_Ub_NDP52_OPTN_p62_N_S_diTBK1i, 1}}},
+  {"T14", 0.01, false, true, {{S_Gal8_Ub_NDP52_OPTN_p62_N_S_diTBK1, 1}}, {{S_Gal8_Ub_NDP52_OPTN_p62_N_S, 1}, {TBK1, 1}}},
+  {"T14i", 0.01, false, true, {{S_Gal8_Ub_NDP52_OPTN_p62_N_S_diTBK1i, 1}}, {{S_Gal8_Ub_NDP52_OPTN_p62_N_S, 1}, {TBK1, 1}}},
+  {"T15", 0.01, false, true, {{S_Gal8_Ub_NDP52_OPTN_p62_N_S, 1}, {TBK1, 2}}, {{S_Gal8_Ub_NDP52_p62_OPTNp_N_S_OligoTBK1, 1}}},
+  {"T16", 0.01, false, true, {{LC3_GABARAP, 1}, {S_Gal8_Ub_NDP52_p62_OPTNp_N_S_OligoTBK1, 1}}, {{Ap_Gal8_Ub_OPTNp, 1}}},
+  {"T16i", 0.01, false, true, {{S_Gal8_Ub_NDP52_OPTN_p62_N_S, 1}, {LC3_GABARAP, 1}}, {{Ap_Gal8_Ub_N_S, 1}}},
+  {"T16ii", 0.01, false, true, {{S_Gal8_Ub_NDP52_OPTN_p62, 1}, {LC3_GABARAP, 1}}, {{Ap_Gal8_Ub, 1}}},
+  {"T18", 0.01, false, true, {{E3_ligase, 1}, {S_damagedSCV, 1}}, {{S_Ub, 1}}},
+  {"T19", 0.01, false, true, {{S_Ub, 1}, {p62, 1}}, {{S_Ub_p62, 1}}},
+  {"T2", 0.01, false, true, {{E3_ligase, 1}, {S_Gal8, 1}}, {{S_Gal8_Ub, 1}}},
+  {"T20", 0.01, false, true, {{S_Ub_p62, 1}}, {{S_Ub, 1}, {p62, 1}}},
+  {"T21", 0.01, false, true, {{S_Ub, 1}, {OPTN, 1}}, {{S_Ub_OPTN, 1}}},
+  {"T22", 0.01, false, true, {{S_Ub_OPTN, 1}}, {{S_Ub, 1}, {OPTN, 1}}},
+  {"T23", 0.01, false, true, {{NDP52, 1}, {S_Ub, 1}}, {{S_Ub_NDP52, 1}}},
+  {"T24", 0.01, false, true, {{S_Ub_NDP52, 1}}, {{NDP52, 1}, {S_Ub, 1}}},
+  {"T25", 0.01, false, true, {{S_Ub, 1}, {OPTN, 1}, {p62, 1}, {NDP52, 1}}, {{S_Ub_NDP52_OPTN_p62, 1}}},
+  {"T26", 0.01, false, true, {{N_S, 1}, {S_Ub_NDP52, 1}}, {{S_Ub_NDP52_N_S, 1}}},
+  {"T26i", 0.01, false, true, {{N_S, 1}, {S_Ub_NDP52_OPTN_p62, 1}}, {{S_Ub_NDP52_OPTN_p62_N_S, 1}}},
+  {"T27", 0.01, false, true, {{S_Ub_NDP52_N_S, 1}}, {{N_S, 1}, {S_Ub, 1}}},
+  {"T28", 0.01, false, true, {{N_S, 1}, {S_Ub, 1}, {OPTN, 1}, {p62, 1}}, {{S_Ub_NDP52_OPTN_p62_N_S, 1}}},
+  {"T29", 0.01, false, true, {{TBK1, 2}, {S_Ub_NDP52_OPTN_p62_N_S, 1}}, {{S_Ub_NDP52_OPTN_p62_N_S_diTBK1, 1}}},
+  {"T29_", 0.01, false, true, {{TBK1, 2}, {S_Ub_NDP52_OPTN_p62_N_S, 1}}, {{S_Ub_NDP52_OPTN_p62_N_S_diTBK1i, 1}}},
+  {"T2i", 0.01, false, true, {{S_Gal8_NDP52, 1}, {E3_ligase, 1}}, {{S_Gal8_Ub_NDP52, 1}}},
+  {"T3", 0.01, false, true, {{NDP52, 1}, {S_Gal8_Ub, 1}}, {{S_Gal8_Ub_NDP52, 1}}},
+  {"T3_release", 0.01, false, true, {{S_Gal8_Ub_NDP52, 1}}, {{S_Gal8_Ub, 1}, {NDP52, 1}}},
+  {"T30", 0.01, false, true, {{S_Ub_NDP52_OPTN_p62_N_S_diTBK1, 1}}, {{S_Ub_NDP52_OPTN_p62_N_S, 1}, {TBK1, 1}}},
+  {"T30_", 0.01, false, true, {{S_Ub_NDP52_OPTN_p62_N_S_diTBK1i, 1}}, {{S_Ub_NDP52_OPTN_p62_N_S, 1}, {TBK1, 1}}},
+  {"T31", 0.01, false, true, {{TBK1, 2}, {S_Ub_NDP52_OPTN_p62_N_S, 1}}, {{S_Ub_NDP52_OPTN_p62_N_S_OligoTBK1, 1}}},
+  {"T32", 0.01, false, true, {{LC3_GABARAP, 1}, {S_Ub_NDP52_OPTN_p62_N_S_OligoTBK1, 1}}, {{Ap_Ub_OPTNp, 1}}},
+  {"T32i", 0.01, false, true, {{LC3_GABARAP, 1}, {S_Ub_NDP52_OPTN_p62_N_S, 1}}, {{Ap_Ub_N_S, 1}}},
+  {"T32ii", 0.01, false, true, {{S_Ub_NDP52_OPTN_p62, 1}, {LC3_GABARAP, 1}}, {{Ap_Ub, 1}}},
+  {"T33", 0.01, false, true, {{SignalSCVdamage, 1}, {AA, 1}}, {{AAstarvation, 1}}},
+  {"T34", 0.01, false, true, {{AAstarvation, 1}, {mTORC1_ULK1comp, 1}}, {{mTORC1inactive, 1}, {ULK1comp, 1}}},
+  {"T35", 0.01, false, true, {{mTORC1inactive, 1}, {ULK1comp, 1}}, {{AA, 1}, {mTORC1_ULK1comp, 1}, {mTORC1_ULK1comp_SCV, 1}}},
+  {"T36", 0.01, false, true, {{ULK1comp, 1}}, {{SignalAutophagyInduction, 1}}},
+  {"T37", 0.01, false, true, {{SignalAutophagyInduction, 1}}, {{ULK1comp, 1}, {LC3_signal, 1}}},
+  {"T3i", 0.01, false, true, {{NDP52, 1}, {S_Gal8, 1}}, {{S_Gal8_NDP52, 1}}},
+  {"T4", 0.01, false, true, {{S_Gal8_NDP52, 1}, {LC3_GABARAP, 1}}, {{Ap_Gal8, 1}}},
+  {"T5", 0.01, false, true, {{OPTN, 1}, {S_Gal8_Ub, 1}}, {{S_Gal8_Ub_OPTN, 1}}},
+  {"T6", 0.01, false, true, {{S_Gal8_Ub_OPTN, 1}}, {{S_Gal8_Ub, 1}, {OPTN, 1}}},
+  {"T7", 0.01, false, true, {{S_Gal8_Ub, 1}, {p62, 1}}, {{S_Gal8_Ub_p62, 1}}},
+  {"T8", 0.01, false, true, {{S_Gal8_Ub_p62, 1}}, {{S_Gal8_Ub, 1}, {p62, 1}}},
+  {"T9", 0.01, false, true, {{p62, 1}, {S_Gal8_Ub, 1}, {NDP52, 1}, {OPTN, 1}}, {{S_Gal8_Ub_NDP52_OPTN_p62, 1}}},
+  {"Bridge", 1.0, true, true, {{XenoStart, 1}, {XenoSig, 1}}, {{S_damagedSCV, 1}, {SignalSCVdamage, 1}}},
+  {"Syn10", 1.0, true, true, {}, {{LC3, 1}}},
+  {"T38", 0.01, false, true, {{LC3_signal, 1}, {LC3, 1}}, {{LC3_GABARAP, 1}}},
+  {"XenoDeg3", 0.0001, true, true, {{SalCyt, 1}}, {{XenoStart, 1}}},
+  {"XenoDeg4", 0.0001, true, true, {{AdapSalCyt, 1}}, {{XenoStart, 1}}},
+  {"XenoFail", 1.0, true, true, {{XenoStart, 1}}, {{SalCyt, 1}}},
+}};
+Marking initial_marking() {
+  Marking m{};
+  m[SalRuffle] = 100;
+  m[SalVac] = 0;
+  m[SalCyt] = 0;
+  m[AdapSalCyt] = 0;
+  m[CapCyt] = 700;
+  m[AdapSalVac] = 0;
+  m[CapVac] = 150;
+  m[XenoSig] = 0;
+  m[AA] = 1;
+  m[AAstarvation] = 0;
+  m[Ap_Gal8] = 0;
+  m[Ap_Gal8_Ub] = 0;
+  m[Ap_Gal8_Ub_N_S] = 0;
+  m[Ap_Gal8_Ub_OPTNp] = 0;
+  m[Ap_Ub] = 0;
+  m[Ap_Ub_N_S] = 0;
+  m[Ap_Ub_OPTNp] = 0;
+  m[E3_ligase] = 50;
+  m[Gal8] = 100;
+  m[LC3_GABARAP] = 50;
+  m[N_S] = 30;
+  m[NDP52] = 100;
+  m[OPTN] = 50;
+  m[S_damagedSCV] = 0;
+  m[S_Gal8] = 0;
+  m[S_Gal8_NDP52] = 0;
+  m[S_Gal8_Ub] = 0;
+  m[S_Gal8_Ub_NDP52] = 0;
+  m[S_Gal8_Ub_NDP52_N_S] = 0;
+  m[S_Gal8_Ub_NDP52_OPTN_p62] = 0;
+  m[S_Gal8_Ub_NDP52_OPTN_p62_N_S] = 0;
+  m[S_Gal8_Ub_NDP52_OPTN_p62_N_S_diTBK1] = 0;
+  m[S_Gal8_Ub_NDP52_OPTN_p62_N_S_diTBK1i] = 0;
+  m[S_Gal8_Ub_NDP52_p62_OPTNp_N_S_OligoTBK1] = 0;
+  m[S_Gal8_Ub_OPTN] = 0;
+  m[S_Gal8_Ub_p62] = 0;
+  m[S_Ub] = 0;
+  m[S_Ub_NDP52] = 0;
+  m[S_Ub_NDP52_N_S] = 0;
+  m[S_Ub_NDP52_OPTN_p62] = 0;
+  m[S_Ub_NDP52_OPTN_p62_N_S] = 0;
+  m[S_Ub_NDP52_OPTN_p62_N_S_OligoTBK1] = 0;
+  m[S_Ub_NDP52_OPTN_p62_N_S_diTBK1] = 0;
+  m[S_Ub_NDP52_OPTN_p62_N_S_diTBK1i] = 0;
+  m[S_Ub_OPTN] = 0;
+  m[S_Ub_p62] = 0;
+  m[SignalAutophagyInduction] = 0;
+  m[SignalSCVdamage] = 0;
+  m[TBK1] = 50;
+  m[ULK1comp] = 0;
+  m[mTORC1_ULK1comp] = 1;
+  m[mTORC1_ULK1comp_SCV] = 0;
+  m[mTORC1inactive] = 0;
+  m[p62] = 50;
+  m[LC3_signal] = 0;
+  m[LC3] = 0;
+  m[XenoStart] = 0;
+  return m;
+}
+double expression_propensity(std::size_t i, const Marking& m) {
+  switch (i) {
+    case 2: return ((0.000105 * m[AdapSalVac]) * (1.0 - (m[AdapSalVac] / 150.0)));
+    case 3: return ((0.000105 * m[AdapSalCyt]) * (1.0 - (m[AdapSalCyt] / 700.0)));
+    case 6: return (((0.0001 * m[SalCyt]) * 100.0) / (100.0 + m[XenoSig]));
+    case 8: return (((0.0001 * m[AdapSalCyt]) * 100.0) / (100.0 + m[XenoSig]));
+    case 17: return ((0.001 * 200.0) / (200.0 + m[Gal8]));
+    case 18: return ((0.001 * 200.0) / (200.0 + m[E3_ligase]));
+    case 19: return ((0.001 * 200.0) / (200.0 + m[p62]));
+    case 20: return ((0.001 * 200.0) / (200.0 + m[NDP52]));
+    case 21: return ((0.001 * 200.0) / (200.0 + m[OPTN]));
+    case 22: return ((0.001 * 200.0) / (200.0 + m[N_S]));
+    case 23: return ((0.001 * 200.0) / (200.0 + m[TBK1]));
+    case 24: return ((0.001 * 200.0) / (200.0 + m[mTORC1_ULK1comp]));
+    case 74: return (0.01 * std::max((m[XenoSig] - 200.0), 0.0));
+    case 75: return ((0.001 * 200.0) / (200.0 + m[LC3_GABARAP]));
+    case 77: return (((((0.001 * m[SalCyt]) * std::min(m[Gal8], 1.0)) * std::min(m[E3_ligase], 1.0)) * std::max((m[XenoSig] - 200.0), 0.0)) / (300.0 + std::max((m[XenoSig] - 200.0), 0.0)));
+    case 78: return (((((0.001 * m[AdapSalCyt]) * std::min(m[Gal8], 1.0)) * std::min(m[E3_ligase], 1.0)) * std::max((m[XenoSig] - 200.0), 0.0)) / (300.0 + std::max((m[XenoSig] - 200.0), 0.0)));
+    case 79: return (0.01 * std::max((m[XenoSig] - 200.0), 0.0));
+    default: return 0.0;
+  }
+}
+}
---
+++ custom_modules/generated/xenophagy_model_generated.h
@@ -0,0 +1,88 @@
+// Generated file. Do not edit.
+// generator=1.0.0 model_sha256=beaafd39036a29aab6eb846082b6d4f2ceef830d7c2d83594d976bd681b5c305 integration_sha256=2d12690ac472de15b05f476dfa7e5e2b6603d042865fc20d6e768d506189c807
+#pragma once
+#include <array>
+#include <cstddef>
+#include <string>
+#include <vector>
+namespace xenophagy {
+enum Place : std::size_t {
+  SalRuffle = 0,
+  SalVac = 1,
+  SalCyt = 2,
+  AdapSalCyt = 3,
+  CapCyt = 4,
+  AdapSalVac = 5,
+  CapVac = 6,
+  XenoSig = 7,
+  AA = 8,
+  AAstarvation = 9,
+  Ap_Gal8 = 10,
+  Ap_Gal8_Ub = 11,
+  Ap_Gal8_Ub_N_S = 12,
+  Ap_Gal8_Ub_OPTNp = 13,
+  Ap_Ub = 14,
+  Ap_Ub_N_S = 15,
+  Ap_Ub_OPTNp = 16,
+  E3_ligase = 17,
+  Gal8 = 18,
+  LC3_GABARAP = 19,
+  N_S = 20,
+  NDP52 = 21,
+  OPTN = 22,
+  S_damagedSCV = 23,
+  S_Gal8 = 24,
+  S_Gal8_NDP52 = 25,
+  S_Gal8_Ub = 26,
+  S_Gal8_Ub_NDP52 = 27,
+  S_Gal8_Ub_NDP52_N_S = 28,
+  S_Gal8_Ub_NDP52_OPTN_p62 = 29,
+  S_Gal8_Ub_NDP52_OPTN_p62_N_S = 30,
+  S_Gal8_Ub_NDP52_OPTN_p62_N_S_diTBK1 = 31,
+  S_Gal8_Ub_NDP52_OPTN_p62_N_S_diTBK1i = 32,
+  S_Gal8_Ub_NDP52_p62_OPTNp_N_S_OligoTBK1 = 33,
+  S_Gal8_Ub_OPTN = 34,
+  S_Gal8_Ub_p62 = 35,
+  S_Ub = 36,
+  S_Ub_NDP52 = 37,
+  S_Ub_NDP52_N_S = 38,
+  S_Ub_NDP52_OPTN_p62 = 39,
+  S_Ub_NDP52_OPTN_p62_N_S = 40,
+  S_Ub_NDP52_OPTN_p62_N_S_OligoTBK1 = 41,
+  S_Ub_NDP52_OPTN_p62_N_S_diTBK1 = 42,
+  S_Ub_NDP52_OPTN_p62_N_S_diTBK1i = 43,
+  S_Ub_OPTN = 44,
+  S_Ub_p62 = 45,
+  SignalAutophagyInduction = 46,
+  SignalSCVdamage = 47,
+  TBK1 = 48,
+  ULK1comp = 49,
+  mTORC1_ULK1comp = 50,
+  mTORC1_ULK1comp_SCV = 51,
+  mTORC1inactive = 52,
+  p62 = 53,
+  LC3_signal = 54,
+  LC3 = 55,
+  XenoStart = 56,
+  PLACE_COUNT = 57
+};
+using Marking = std::array<int, PLACE_COUNT>;
+struct ModelParameters {
+  double k_death = 1e-07;
+  double death_threshold = 100.0;
+  double cap_cyt_initial = 700.0;
+  double cap_vac_initial = 150.0;
+  double sigmoid_k = 5e-05;
+  double sigmoid_mid_base = 1800.0;
+  double sigmoid_mid_slope = 200.0;
+  double mhc_alpha = 1.0;
+  double mhc_beta = 1.0;
+};
+struct Arc { Place place; int weight; };
+struct Transition { const char* id; double rate; bool uses_expression; bool enabled; std::vector<Arc> input; std::vector<Arc> output; };
+extern const ModelParameters parameters;
+extern const std::array<const char*, PLACE_COUNT> place_names;
+extern const std::array<Transition, 80> transitions;
+Marking initial_marking();
+double expression_propensity(std::size_t transition_index, const Marking& m);
+}
